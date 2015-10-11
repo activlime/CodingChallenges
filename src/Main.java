138013635cdb1325eec49e43bdb81e041fbac5b3
activlime <choap@usc.edu>
2015-10-11 15:46:00 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,53 @@
+import org.apache.commons.lang.ObjectUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Created by Andrew Cho on 10/11/2015.
+ *
+ * Linkedin interview
+ */
+public class Main {
+    public static void main(String[] args) {
+        wordDistance();
+    }
+
+    public static void wordDistance() {
+        Scanner in = new Scanner(System.in);
+        Map<String, Integer> dict = new HashMap<String, Integer>();
+        int position = 0;
+
+        System.out.println("Input list of words, enter \"done\" when finished inputting:");
+        while (in.hasNext()) {
+            String temp = in.next();
+            if (temp.equals("done")){
+                break;
+            }
+            dict.put(temp, position);
+            position++;
+        }
+
+        String wordOne;
+        String wordTwo;
+
+        System.out.print("Input two words to find distance, first word:");
+        wordOne = in.next();
+        while (dict.get(wordOne) == null) {
+            System.out.print("Existing word not inputted, try again, first word:");
+            wordOne = in.next();
+        }
+
+        System.out.print("Second word:");
+        wordTwo = in.next();
+        while (dict.get(wordTwo) == null) {
+            System.out.print("Existing word not inputted, try again, second word:");
+            wordTwo = in.next();
+        }
+
+        int distance = Math.abs(dict.get(wordOne) - dict.get(wordTwo));
+        System.out.println("Distance of word " + wordOne + " and " + wordTwo +
+        " is " + distance);
+    }
+}
