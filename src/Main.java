52f5e3a80bdd80fe0c9ac0abeba4e937cb68a831
Cho <andrcho@deloitte.com>
2016-02-02 15:34:19 -0800

    implemeneting three array

--- src/Main.java
+++ src/Main.java
@@ -12,7 +12,7 @@ import java.util.*;
  */
 public class Main {
     public static void main(String[] args) {
-        HashMapTest();
+        ThreeArray array = new ThreeArray();
     }
 
     /*
--- src/ThreeArray.java
+++ src/ThreeArray.java
@@ -2,11 +2,12 @@ import java.util.List;
 
 /**
  * Created by andrcho on 1/7/2016.
+ *
+ * Keeps three arrays in one array
+ *
+ * O(1)
  */
 public class ThreeArray {
-    int one = 0;
-    int two = 0;
-    int three = 0;
     int onesize = 0;
     int twosize = 0;
     int threesize = 0;
@@ -15,33 +16,73 @@ public class ThreeArray {
     Integer[] arr = new Integer[bound];
 
     public ThreeArray() {
+        presetArray();
+        addTwo(2);
+        printArray();
+    }
 
+    private void presetArray() {
+        for (int i = 0; i < bound; i++) {
+            arr[i] = 0;
+        }
     }
 
     public void addOne(int num) {
         int size = size();
-        if (size == 0) {
-            arr[size] = num;
-            one++;
-        } else {
-
+        if (size >= bound) {
+            doubleArray();
         }
+
+        arr[size] = arr[onesize];
+
+        int twototal = onesize + twosize;
+        arr[twototal] = arr[size];
+        arr[onesize] = num;
+
+        onesize++;
     }
 
     public void addTwo(int num) {
         int size = size();
         if (size >= bound) {
-           bound *= 2;
+            doubleArray();
         }
 
-        int sizetwo = onesize+twosize;
-        int temp = arr[sizetwo];
-        arr[sizetwo] = num;
+        int twototal = onesize+twosize;
+        int temp = arr[twototal];
+        arr[twototal] = num;
         arr[size] = temp;
         twosize++;
     }
 
+    public void addThree(int num) {
+        int size = size();
+        if (size >= bound) {
+            doubleArray();
+        }
+
+        arr[size] = num;
+        threesize++;
+    }
+
+    private void doubleArray() {
+        Integer[] newArray = new Integer[bound*2];
+        for (int i = 0; i < bound; i++) {
+            newArray[i] = arr[i];
+        }
+
+        arr = newArray;
+
+        bound *= 2;
+    }
+
     private int size() {
         return onesize + twosize + threesize;
+    }
+
+    public void printArray() {
+        for (int i = 0; i < size(); i++) {
+            System.out.println(arr[i]);
+        }
     }
 }
